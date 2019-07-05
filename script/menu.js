58c5e2ba9d88a9f473384f655483a299ab265dc2
irinaWinter <lobanova.irina.96@mail.ru>
2019-07-05 15:02:07 +0300

    menu ВП

---
+++ script/menu.js
@@ -0,0 +1,17 @@
+var button = document.querySelector('.Hamburger');
+button.addEventListener('click', showHideMenu);
+
+
+
+function showHideMenu() {
+	var menu = document.querySelector('.menu'),
+		main = document.querySelector('.main'),
+		footer = document.querySelectorAll('.footer');
+
+	menu.classList.toggle('close');
+	main.classList.toggle('close');
+	footer.forEach(function(element) {
+		console.log(element);
+		element.classList.toggle('close');
+	});
+};

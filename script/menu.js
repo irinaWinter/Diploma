277f593d32dc1aa5f25f3418f2b6748b2a4a9e24
irinaWinter <lobanova.irina.96@mail.ru>
2019-07-08 15:31:36 +0300

    header

--- script/menu.js
+++ script/menu.js
@@ -6,11 +6,19 @@ button.addEventListener('click', showHideMenu);
 function showHideMenu() {
 	var menu = document.querySelector('.menu'),
 		main = document.querySelector('.main'),
-		footer = document.querySelectorAll('.footer');
+		footer = document.querySelectorAll('.footer'),
+		link = document.querySelectorAll('.menu__a');
 
+	button.classList.toggle('change');
 	menu.classList.toggle('close');
 	main.classList.toggle('close');
 	footer.forEach(function(element) {
 		element.classList.toggle('close');
 	});
+	link.forEach(function(element) {
+		element.addEventListener('click', function() {
+			showHideMenu();
+		});
+	});
+
 };

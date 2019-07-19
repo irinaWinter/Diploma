0b5128a9ca760f9affb94214fad819a74c04bf0c
Irina Winter <lobanova.irina.96@mail.ru>
2019-07-19 15:35:04 +0300

    /

---
+++ script/portfolio-data.js
@@ -0,0 +1,8 @@
+var portfolio_data = [
+	{ title: "Сайт магазина детской одежды",
+	  preview: "portfolio1" },
+	{ title: "Сайт психолога Веры Антоновой",
+	  preview: "portfolio2" },
+	{ title: "Сайт магазина \"Рыболов\"",
+	  preview: "portfolio3" }	  
+]
